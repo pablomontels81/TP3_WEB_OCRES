1e9fff8d83b01e5b9dddeb355b59be55ac7e91bb
pablomontels81 <pablo.montels@edu.ece.fr>
2020-10-06 16:57:33 +0200

    Fonction d'actualisation de la météo OK

--- js/script.js
+++ js/script.js
@@ -29,3 +29,34 @@ function start() {
       console.error(error);
     });
 }
+
+// Fonction appelée lors du click du bouton
+function actualisation() {
+  // Création de l'objet apiWeather
+  const apiWeather = new API_WEATHER(document.getElementById('city-input').value);
+  // Appel de la fonction fetchTodayForecast
+
+  apiWeather
+    .fetchTodayForecast()
+    .then(function(response) {
+      // Récupère la donnée d'une API
+      const data = response.data;
+
+      // On récupère l'information principal
+      const main = data.weather[0].main;
+      const description = data.weather[0].description;
+      const temp = data.main.temp;
+      const icon = apiWeather.getHTMLElementFromIcon(data.weather[0].icon);
+
+      // Modifier le DOM
+      document.getElementById('today-forecast-main').innerHTML = main;
+      document.getElementById('today-forecast-more-info').innerHTML = description;
+      document.getElementById('icon-weather-container').innerHTML = icon;
+      document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
+      
+    })
+    .catch(function(error) {
+      // Affiche une erreur
+      console.error(error);
+    });
+}
