5a5c3db53fa57e6ae7cc09a02939ecef6b4265c9
pablomontels81 <pablo.montels@edu.ece.fr>
2020-10-11 18:03:01 +0200

    Mise en place JS pour récupération météo J+1, J+2 & J+3

--- js/API_Weather.js
+++ js/API_Weather.js
@@ -25,6 +25,14 @@ class API_WEATHER{
       crossdomain: true
     })
   }
+
+  fetchThreeDayForcast(){
+    return axios
+    .get(`${API_URL}?q=${this.city}&units=metric&appid=${API_KEY}`, {
+      crossdomain: true
+    })
+  }
+  
   // Retourne l'element HTML de l'icon symbolisant la méteo.
   getHTMLElementFromIcon(icon){
     return `<img src=${API_URL_ICON}${icon}@2x.png class="weather-icon"/>`
--- js/script.js
+++ js/script.js
@@ -28,6 +28,57 @@ function start() {
       // Affiche une erreur
       console.error(error);
     });
+
+  apiWeather
+    .fetchThreeDayForcast()
+    .then(function(response)) {
+      //Récupère les données de l'API pour J+1, J+2, J+3
+      
+      //Jour 1
+      const dataJ1 = response.data.list[0];
+
+      // On récupère l'information principal
+      const mainJ1 = dataJ1.weather[0].main;
+      const descriptionJ1 = dataJ1.weather[0].description;
+      const tempJ1 = dataJ1.main.temp;
+      const iconJ1 = apiWeather.getHTMLElementFromIcon(dataJ1.weather[0].icon);
+
+      // Modifier le DOM
+      document.getElementById('today-forecast-main').innerHTML = mainJ1;
+      document.getElementById('today-forecast-more-info').innerHTML = descriptionJ1;
+      document.getElementById('icon-weather-container').innerHTML = iconJ1;
+      document.getElementById('today-forecast-temp').innerHTML = `${tempJ1}°C`;
+
+      //Jour 2
+      const dataJ2 = response.data.list[1];
+
+      // On récupère l'information principal
+      const mainJ2 = dataJ2.weather[1].main;
+      const descriptionJ2 = dataJ2.weather[1].description;
+      const tempJ2 = dataJ2.main.temp;
+      const iconJ2 = apiWeather.getHTMLElementFromIcon(dataJ2.weather[1].icon);
+
+      // Modifier le DOM
+      document.getElementById('today-forecast-main').innerHTML = mainJ2;
+      document.getElementById('today-forecast-more-info').innerHTML = descriptionJ2;
+      document.getElementById('icon-weather-container').innerHTML = iconJ2;
+      document.getElementById('today-forecast-temp').innerHTML = `${tempJ2}°C`;
+
+      //Jour 3
+      const dataJ3 = response.data.list[2];
+
+      // On récupère l'information principal
+      const mainJ3 = dataJ3.weather[2].main;
+      const descriptionJ3 = dataJ3.weather[2].description;
+      const tempJ3 = dataJ3.main.temp;
+      const iconJ3 = apiWeather.getHTMLElementFromIcon(dataJ3.weather[2].icon);
+
+      // Modifier le DOM
+      document.getElementById('today-forecast-main').innerHTML = mainJ3;
+      document.getElementById('today-forecast-more-info').innerHTML = descriptionJ3;
+      document.getElementById('icon-weather-container').innerHTML = iconJ3;
+      document.getElementById('today-forecast-temp').innerHTML = `${tempJ3}°C`;
+    }
 }
 
 // Fonction appelée lors du click du bouton
