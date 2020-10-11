fd6d54f602fc218f6839c74ed84e9ecd57613f7c
pablomontels81 <pablo.montels@edu.ece.fr>
2020-10-11 18:17:26 +0200

    Erreur sur le catch !

--- js/script.js
+++ js/script.js
@@ -44,10 +44,10 @@ function start() {
       const iconJ1 = apiWeather.getHTMLElementFromIcon(dataJ1.weather[0].icon);
 
       // Modifier le DOM
-      document.getElementById('today-forecast-main').innerHTML = mainJ1;
-      document.getElementById('today-forecast-more-info').innerHTML = descriptionJ1;
-      document.getElementById('icon-weather-container').innerHTML = iconJ1;
-      document.getElementById('today-forecast-temp').innerHTML = `${tempJ1}°C`;
+      document.getElementById('today-forecast-main-1').innerHTML = mainJ1;
+      document.getElementById('today-forecast-more-info-1').innerHTML = descriptionJ1;
+      document.getElementById('icon-weather-container-1').innerHTML = iconJ1;
+      document.getElementById('today-forecast-temp-1').innerHTML = `${tempJ1}°C`;
 
       //Jour 2
       const dataJ2 = response.data.list[1];
@@ -59,10 +59,10 @@ function start() {
       const iconJ2 = apiWeather.getHTMLElementFromIcon(dataJ2.weather[1].icon);
 
       // Modifier le DOM
-      document.getElementById('today-forecast-main').innerHTML = mainJ2;
-      document.getElementById('today-forecast-more-info').innerHTML = descriptionJ2;
-      document.getElementById('icon-weather-container').innerHTML = iconJ2;
-      document.getElementById('today-forecast-temp').innerHTML = `${tempJ2}°C`;
+      document.getElementById('today-forecast-main-2').innerHTML = mainJ2;
+      document.getElementById('today-forecast-more-info-2').innerHTML = descriptionJ2;
+      document.getElementById('icon-weather-container-2').innerHTML = iconJ2;
+      document.getElementById('today-forecast-temp-2').innerHTML = `${tempJ2}°C`;
 
       //Jour 3
       const dataJ3 = response.data.list[2];
@@ -74,11 +74,12 @@ function start() {
       const iconJ3 = apiWeather.getHTMLElementFromIcon(dataJ3.weather[2].icon);
 
       // Modifier le DOM
-      document.getElementById('today-forecast-main').innerHTML = mainJ3;
-      document.getElementById('today-forecast-more-info').innerHTML = descriptionJ3;
-      document.getElementById('icon-weather-container').innerHTML = iconJ3;
-      document.getElementById('today-forecast-temp').innerHTML = `${tempJ3}°C`;
+      document.getElementById('today-forecast-main-3').innerHTML = mainJ3;
+      document.getElementById('today-forecast-more-info-3').innerHTML = descriptionJ3;
+      document.getElementById('icon-weather-container-3').innerHTML = iconJ3;
+      document.getElementById('today-forecast-temp-3').innerHTML = `${tempJ3}°C`;
     })
+    
     .catch(function(error) {
       // Affiche une erreur
       console.error(error);
@@ -115,6 +116,7 @@ function actualisation() {
       console.error(error);
     });
 
+
     apiWeather
     .fetchThreeDayForcast()
     .then(function(response) {
@@ -130,10 +132,10 @@ function actualisation() {
       const iconJ1 = apiWeather.getHTMLElementFromIcon(dataJ1.weather[0].icon);
 
       // Modifier le DOM
-      document.getElementById('today-forecast-main').innerHTML = mainJ1;
-      document.getElementById('today-forecast-more-info').innerHTML = descriptionJ1;
-      document.getElementById('icon-weather-container').innerHTML = iconJ1;
-      document.getElementById('today-forecast-temp').innerHTML = `${tempJ1}°C`;
+      document.getElementById('today-forecast-main-1').innerHTML = mainJ1;
+      document.getElementById('today-forecast-more-info-1').innerHTML = descriptionJ1;
+      document.getElementById('icon-weather-container-1').innerHTML = iconJ1;
+      document.getElementById('today-forecast-temp-1').innerHTML = `${tempJ1}°C`;
 
       //Jour 2
       const dataJ2 = response.data.list[1];
@@ -145,10 +147,10 @@ function actualisation() {
       const iconJ2 = apiWeather.getHTMLElementFromIcon(dataJ2.weather[1].icon);
 
       // Modifier le DOM
-      document.getElementById('today-forecast-main').innerHTML = mainJ2;
-      document.getElementById('today-forecast-more-info').innerHTML = descriptionJ2;
-      document.getElementById('icon-weather-container').innerHTML = iconJ2;
-      document.getElementById('today-forecast-temp').innerHTML = `${tempJ2}°C`;
+      document.getElementById('today-forecast-main-2').innerHTML = mainJ2;
+      document.getElementById('today-forecast-more-info-2').innerHTML = descriptionJ2;
+      document.getElementById('icon-weather-container-2').innerHTML = iconJ2;
+      document.getElementById('today-forecast-temp-2').innerHTML = `${tempJ2}°C`;
 
       //Jour 3
       const dataJ3 = response.data.list[2];
@@ -160,11 +162,12 @@ function actualisation() {
       const iconJ3 = apiWeather.getHTMLElementFromIcon(dataJ3.weather[2].icon);
 
       // Modifier le DOM
-      document.getElementById('today-forecast-main').innerHTML = mainJ3;
-      document.getElementById('today-forecast-more-info').innerHTML = descriptionJ3;
-      document.getElementById('icon-weather-container').innerHTML = iconJ3;
-      document.getElementById('today-forecast-temp').innerHTML = `${tempJ3}°C`;
+      document.getElementById('today-forecast-main-3').innerHTML = mainJ3;
+      document.getElementById('today-forecast-more-info-3').innerHTML = descriptionJ3;
+      document.getElementById('icon-weather-container-3').innerHTML = iconJ3;
+      document.getElementById('today-forecast-temp-3').innerHTML = `${tempJ3}°C`;
     })
+
     .catch(function(error) {
       // Affiche une erreur
       console.error(error);
